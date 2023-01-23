bec2a8343567286ff05de2838291fcabebebc8ae
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-01-23 23:07:19 +0530

    Important Sorting Algorithms

--- Sorting_Algorithms/MergeSort.java
+++ Sorting_Algorithms/MergeSort.java
@@ -1,3 +1,61 @@
 public class MergeSort {
-    
+
+    // This function is used to perform the main task of the merge sort i.e. merging the sorted arrays.
+    public static void conquer(int[] arr, int si, int mid, int ei){
+        int merged[] = new int[ei - si + 1];  // New array after merging
+        int idx1 = si;  // Sorted array's(before mid and including mid) first index 
+        int idx2 = mid + 1;  // Sorted array's(after mid) first index
+        int x = 0;  // Starting index of mergred array
+
+        // We will compare the elements of first and second sorted array and put them in the merged array accordingly while either of them reaches the end.
+        while(idx1 <= mid && idx2 <= ei){
+            if(arr[idx1] <= arr[idx2]){
+                merged[x++] = arr[idx1++];
+            }
+            else{
+                merged[x++] = arr[idx2++];
+            }
+        }
+
+        // Only one of the below while loops will run because if both the conditions are true then the above while loop will not stop.
+
+        // Put all the remaining elements of first sorted array in the merged array.
+        while(idx1 <= mid){
+            merged[x++] = arr[idx1++];
+        }
+        
+        // Put all the remaining elements of second sorted array in the merged array.
+        while(idx2 <= ei){
+            merged[x++] = arr[idx2++];
+        }
+
+        // Restoring the elements of the original array according to the merged array. 
+        for(int i = 0, j = si; i < merged.length; i++, j++){
+            arr[j] = merged[i];
+        }
+
+    }
+
+    // This function is used to divide the array into its smallest possible sorted form.
+    public static void divide(int[] arr, int si, int ei){
+        if(si >= ei) return;
+
+        int mid = si + (ei - si)/2;
+        divide(arr, si, mid);
+        divide(arr, mid + 1, ei);
+        conquer(arr, si, mid, ei);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6, 3, 3, 2, 5, 8};
+        int n = arr.length;
+
+        divide(arr, 0, n - 1);
+
+        // Print the sorted array
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
--- Sorting_Algorithms/QuickSort.java
+++ Sorting_Algorithms/QuickSort.java
@@ -1,3 +1,49 @@
 public class QuickSort {
+
+    public static int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for(int j = low; j < high; j++){
+            if(arr[j] < pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Now we have to place the pivot
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+
+        return i;  // i here basically represents the pivot index. Now all the elements before pivot are smaller than pivot and all the elements after the pivot are greater than pivot. So this is the fixed position of the pivot in the sorted array.
+    }
+
+    public static void quickSort(int[] arr, int low, int high){
+        if(low < high){
+            int pivotIndex = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    // Time Complexity in worst case is -> O(n^2) : This is the case when the pivot is always the largest or smallest element of the array. Basically when the array is already sorted in either ascending or descending order.
+    // Time Complexity in average case is -> O(nlogn)
     
+    public static void main(String[] args) {
+        int[] arr = {6, 3, 9, 2, 5, 8};
+        int n = arr.length;
+
+        quickSort(arr, 0, n - 1);
+
+        // Print the sorted array
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
