235ad9b155d3f4f660e0e2c9f2a193251d591588
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-04-05 23:10:48 +0530

    Print All Paths From Source To Destination

--- Graph/AllPaths.java
+++ Graph/AllPaths.java
@@ -3,6 +3,7 @@ import java.util.*;;
 
 public class AllPaths {
 
+    // Creating a Edge class to store the source, destination.
     static class Edge{
         int src;
         int des;
@@ -13,67 +14,46 @@ public class AllPaths {
         }
     }
 
-    public static void createGraph(ArrayList<Edge> graph[]){
-        for(int i = 0; i < graph.length; i++){
-            graph[i] = new ArrayList<>();
+    public static List<List<Edge>> createGraph(int n, int[][] edges){
+
+        List<List<Edge>> graph = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            graph.add(new ArrayList<>());
         }
 
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
-        
-        graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 3));
+        for(int i = 0; i < edges.length; i++){
+            int src = edges[i][0];
+            int des = edges[i][1];
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 4));
-
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 4));
-        graph[3].add(new Edge(3, 5));
-
-        graph[4].add(new Edge(4, 2));
-        graph[4].add(new Edge(4, 3));
-        graph[4].add(new Edge(4, 5));
-
-        graph[5].add(new Edge(5, 3));
-        graph[5].add(new Edge(5, 4));
-        graph[5].add(new Edge(5, 6));
-
-        graph[6].add(new Edge(6, 5));
-    }
-
-    public static void dfs(ArrayList<Edge> graph[], int curr, boolean visited[]){
-        System.out.print(curr + " ");
-        visited[curr] = true;
-
-        for(int i = 0; i < graph[curr].size(); i++){
-            Edge e = graph[curr].get(i);
-            if(visited[e.des] == false) dfs(graph, e.des, visited);
+            graph.get(src).add(new Edge(src, des));
+            graph.get(des).add(new Edge(des, src));
         }
+
+        return graph;
     }
 
     // Time Complexity -> O(V^V) where V is No. of Vertices
-    public static void printAllPaths(ArrayList<Edge> graph[], int curr, boolean visited[], String path, int tar){
+    public static void printAllPaths(List<List<Edge>> graph, int curr, boolean visited[], String path, int tar){
         if(curr == tar){
             System.out.println(path);
             return;
         }
 
-        for(int i = 0; i < graph[curr].size(); i++){
-            Edge e = graph[curr].get(i);
+        for(int i = 0; i < graph.get(curr).size(); i++){
+            Edge e = graph.get(curr).get(i);
             if(!visited[e.des]){
                 visited[curr] = true;
-                printAllPaths(graph, e.des, visited, path + e.des, tar);
+                printAllPaths(graph, e.des, visited, path + e.des + "->", tar);
                 visited[curr] = false;
             }
         }
     }
 
     public static void main(String[] args) {
-        int V = 7;
+        int n = 7;
+        int[][] edges = {{0, 1}, {0, 2}, {1, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 5}, {5, 6}};
 
-        ArrayList<Edge> graph[] = new ArrayList[V];
-        createGraph(graph);
+        List<List<Edge>> graph = createGraph(n, edges);
 
         /*
 
@@ -86,8 +66,8 @@ public class AllPaths {
         */
 
         int src = 0, tar = 5;
-        boolean[] visited = new boolean[V];
-        printAllPaths(graph, src, visited, "0", tar);
+        boolean[] visited = new boolean[n];
+        printAllPaths(graph, src, visited, "0 -> ", tar);
         System.out.println();
     }
 }
