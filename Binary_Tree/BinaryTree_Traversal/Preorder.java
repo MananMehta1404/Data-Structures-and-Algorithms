2688d4b72deae7ffe15f120f6fd3273f1d73ec1b
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-04-18 20:16:58 +0530

    Preorder traversal of a binary tree

---
+++ Binary_Tree/BinaryTree_Traversal/Preorder.java
@@ -0,0 +1,53 @@
+package Binary_Tree.BinaryTree_Traversal;
+
+public class Preorder {
+
+    // Class to define the node structure of a binary tree
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Class to build the binary tree
+    static class BinaryTree{
+        static int idx = -1;
+
+        // Function to build the binary tree
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1) return null;
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    // Function to print the preorder traversal of the binary tree
+    public static void preOrder(Node root){
+        if(root == null) return;
+
+        System.out.print(root.data + " ");
+
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, -1};
+        Node root = BinaryTree.buildTree(nodes);
+        
+        System.out.println("Preorder traversal of the binary tree is: ");
+        preOrder(root);
+    }
+    
+}
