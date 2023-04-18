c3a3ab31fe1fb9cb9e908c593e9ad3f0824bfa32
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-04-18 20:17:44 +0530

    Levelorder traversal of a binary tree

---
+++ Binary_Tree/BinaryTree_Traversal/Levelorder.java
@@ -0,0 +1,70 @@
+package Binary_Tree.BinaryTree_Traversal;
+
+import java.util.*;
+
+public class Levelorder {
+
+    // Class to define the node structure of a binary tree
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Class to build the binary tree
+    static class BinaryTree{
+        static int idx = -1;
+
+        // Function to build the binary tree
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1) return null;
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void levelOrder(Node root){
+        if(root == null) return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Node currNode = q.remove();
+            if(currNode == null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
+            }
+            else{
+                System.out.print(currNode.data + " ");
+                if(currNode.left != null) q.add(currNode.left);
+                if(currNode.right != null) q.add(currNode.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, -1};
+        Node root = BinaryTree.buildTree(nodes);
+        
+        System.out.println("Levelorder traversal of the binary tree is: ");
+        levelOrder(root);
+    }
+    
+}
