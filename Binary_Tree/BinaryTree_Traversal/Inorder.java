4df3ef8e70e037d963237d851c54ddd2dd692dd8
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-04-18 20:17:12 +0530

    Inorder traversal of a binary tree

---
+++ Binary_Tree/BinaryTree_Traversal/Inorder.java
@@ -0,0 +1,50 @@
+package Binary_Tree.BinaryTree_Traversal;
+
+public class Inorder {
+
+    // Class to define the node structure of a binary tree
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Class to build the binary tree
+    static class BinaryTree{
+        static int idx = -1;
+
+        // Function to build the binary tree
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1) return null;
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void inOrder(Node root){
+        if(root == null) return;
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, -1};
+        Node root = BinaryTree.buildTree(nodes);
+        
+        System.out.println("Inorder traversal of the binary tree is: ");
+        inOrder(root);
+    }
+    
+}
