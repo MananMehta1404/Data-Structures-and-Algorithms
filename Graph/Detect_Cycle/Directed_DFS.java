d4e9ac187462f96045af9301945b47afca360fe4
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-04-22 20:15:39 +0530

    Detect cycle in directed graph using DFS

---
+++ Graph/Detect_Cycle/Directed_DFS.java
@@ -0,0 +1,73 @@
+package Graph.Detect_Cycle;
+
+import java.util.*;
+
+public class Directed_DFS {
+
+    // Creating a Edge class to store the source, destination.
+    static class Edge{
+        int src;
+        int des;
+
+        Edge(int s, int d){
+            this.src = s;
+            this.des = d;
+        }
+    }
+
+    public static List<List<Edge>> createGraph(int n, int[][] edges){
+
+        List<List<Edge>> graph = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < edges.length; i++){
+            int src = edges[i][0];
+            int des = edges[i][1];
+
+            graph.get(src).add(new Edge(src, des));
+        }
+
+        return graph;
+    }
+
+    public static boolean dfs(List<List<Edge>> graph, int src, int[] visited, int[] pathVisited){
+        visited[src] = 1;
+        pathVisited[src] = 1;
+
+        for(int i = 0; i < graph.get(src).size(); i++){
+            Edge e = graph.get(src).get(i);
+            if(visited[e.des] == 0){
+                if(dfs(graph, e.des, visited, pathVisited)){
+                    return true;
+                }
+            }
+            else if(pathVisited[e.des] == 1){
+                return true;
+            }
+        }
+
+        pathVisited[src] = 0;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] edges = {{1, 2}, {2, 3}, {3, 4}, {3, 7}, {4, 5}, {5, 6}, {7, 5}, {8, 2}, {8, 9}, {9, 10}, {10, 8}};
+        int n = 11;
+
+        List<List<Edge>> graph = createGraph(n, edges);
+
+        int[] visited = new int[n];
+        int[] pathVisited = new int[n];
+
+        for(int i = 0; i < n; i++){
+            if(visited[i] == 0){
+                if(dfs(graph, i, visited, pathVisited)){
+                    System.out.println("Cycle is present at node: " + i);
+                    break;
+                }
+            }
+        }
+    }
+}
