a1187db6b9c14c44d33e324f59ad9ca9f0479925
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-05-10 12:12:46 +0530

    Topological Sort Using BFS - Kahn's Algorithm

---
+++ Graph/TopoSort_BFS.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class TopoSort_BFS {
+
+    // Creating a Edge class to store the source, destination.
+    static class Edge{
+        int src;
+        int des;
+
+        Edge(int s, int d){
+            this.src = s;
+            this.des = d;
+        }
+    }
+
+    public static List<List<Edge>> createGraph(int n, int[][] edges){
+
+        List<List<Edge>> graph = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < edges.length; i++){
+            int src = edges[i][0];
+            int des = edges[i][1];
+
+            graph.get(src).add(new Edge(src, des));
+        }
+
+        return graph;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[][] edges = {{3, 1}, {2, 3}, {4, 1}, {4, 0}, {5, 2}, {5, 0}};
+
+        List<List<Edge>> graph = createGraph(n, edges);
+
+        int[] indegree = new int[n];
+        for(int i = 0; i < n; i++){
+            for(Edge e : graph.get(i)){
+                indegree[e.des]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                queue.add(i);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            result.add(node);
+
+            for(Edge e : graph.get(node)){
+                indegree[e.des]--;
+                if(indegree[e.des] == 0){
+                    queue.add(e.des);
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
