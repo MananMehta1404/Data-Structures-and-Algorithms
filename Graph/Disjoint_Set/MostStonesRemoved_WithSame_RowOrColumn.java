342be8b0188b7c3d508af2e6a1cb20cd80d007ed
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-06-14 13:14:37 +0530

    Most Stones Removed with Same Row or Column

---
+++ Graph/Disjoint_Set/MostStonesRemoved_WithSame_RowOrColumn.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class MostStonesRemoved_WithSame_RowOrColumn {
+
+    public static int maxRemove(int[][] stones, int n) {
+        int maxRow = 0;
+        int maxCol = 0;
+        for (int i = 0; i < n; i++) {
+            maxRow = Math.max(maxRow, stones[i][0]);
+            maxCol = Math.max(maxCol, stones[i][1]);
+        }
+
+        DisjointSet ds = new DisjointSet(maxRow + maxCol + 1);
+        HashMap<Integer, Integer> stoneNodes = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int nodeRow = stones[i][0];
+            int nodeCol = stones[i][1] + maxRow + 1;
+            ds.unionBySize(nodeRow, nodeCol);
+            stoneNodes.put(nodeRow, 1);
+            stoneNodes.put(nodeCol, 1);
+        }
+
+        int cnt = 0;
+        for (Map.Entry<Integer, Integer> it : stoneNodes.entrySet()) {
+            if (ds.findUPar(it.getKey()) == it.getKey()) {
+                cnt++;
+            }
+        }
+
+        return n - cnt;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[][] stones = { {0, 0}, {0, 2}, {1, 3}, {3, 1}, {3, 2}, {4, 3}};
+
+        int ans = maxRemove(stones, n);
+        System.out.println("The maximum number of stones we can remove is: " + ans);
+    }
+}
+
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
