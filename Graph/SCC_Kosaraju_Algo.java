e9a6b5835df99ab1d774c533c60b54a63538113d
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-06-15 13:41:53 +0530

    Strongly Connected Components - Kosaraju's Algo

---
+++ Graph/SCC_Kosaraju_Algo.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+public class SCC_Kosaraju_Algo {
+
+    public static void dfs(int node, int []vis, List<List<Integer>> adj, Stack<Integer> st) {
+        vis[node] = 1;
+        for (Integer it : adj.get(node)) {
+            if (vis[it] == 0) {
+                dfs(it, vis, adj, st);
+            }
+        }
+        st.push(node);
+    }
+
+    public static void dfs3(int node, int[] vis, List<List<Integer>> adjT) {
+        vis[node] = 1;
+        for (Integer it : adjT.get(node)) {
+            if (vis[it] == 0) {
+                dfs3(it, vis, adjT);
+            }
+        }
+    }
+
+    //Function to find number of strongly connected components in the graph.
+    public static int kosaraju(int V, List<List<Integer>> adj) {
+        int[] vis = new int[V];
+        Stack<Integer> st = new Stack<Integer>();
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0) {
+                dfs(i, vis, adj, st);
+            }
+        }
+
+        List<List<Integer>> adjT = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adjT.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < V; i++) {
+            vis[i] = 0;
+            for (Integer it : adj.get(i)) {
+                // i -> it
+                // it -> i
+                adjT.get(it).add(i);
+            }
+        }
+
+        int scc = 0;
+        while (!st.isEmpty()) {
+            int node = st.peek();
+            st.pop();
+            if (vis[node] == 0) {
+                scc++;
+                dfs3(node, vis, adjT);
+            }
+        }
+        
+        return scc;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] edges = {{1, 0}, {0, 2}, {2, 1}, {0, 3}, {3, 4}};
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < n; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+        }
+
+        int ans = kosaraju(n, adj);
+        System.out.println("The number of strongly connected components is: " + ans);
+    }
+}
