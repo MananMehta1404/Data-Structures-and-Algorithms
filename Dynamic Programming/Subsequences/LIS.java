f5b6e3dd64451be15fb0f50e830721305335b36c
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-07-05 12:49:01 +0530

    LIS (Algorithmic Approach)

--- Dynamic Programming/Subsequences/LIS.java
+++ Dynamic Programming/Subsequences/LIS.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 
 public class LIS {
+
+    // Memoization Approach
     public static int lengthOfLIS(int idx, int prev_idx, int[] arr, int[][] dp) {
         if(idx == arr.length) return 0;
 
@@ -13,6 +15,25 @@ public class LIS {
 
         return dp[idx][prev_idx + 1] = len;
     }
+
+    // Tabulation Approach
+    public static int lenOfLIS(int[] arr, int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        int maxlen = 0;
+        for(int i = 0; i < n; i++) {
+            for(int prev_idx = 0; prev_idx < i; prev_idx++) {
+                if(arr[prev_idx] < arr[i] && 1 + dp[prev_idx] > dp[i]){
+                    dp[i] = 1 + dp[prev_idx];
+                }
+            }
+            maxlen = Math.max(maxlen, dp[i]);
+        }
+
+        return maxlen;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 10, 22, 9, 33, 21, 50, 41, 60, 80 };
         int n = arr.length;
@@ -22,5 +43,7 @@ public class LIS {
 
         int ans = lengthOfLIS(0, -1, arr, dp);
         System.out.println(ans);
+
+        System.out.println("Maximum length is: " + lenOfLIS(arr, n));
     }
 }
