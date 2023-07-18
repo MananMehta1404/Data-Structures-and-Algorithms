fed2b8087a7dc8967fa8fae05b62c11bb07f1e8f
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-07-18 22:45:38 +0530

    LRU Cache

---
+++ Queue/LRU_Cache.java
@@ -0,0 +1,77 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRU_Cache {
+
+    Node head = new Node(0, 0);
+    Node tail = new Node(0, 0);
+    Map<Integer, Node> map = new HashMap<>();
+    int capacity;
+
+    class Node {
+        Node next, prev;
+        int key, value;
+        
+        Node (int _key, int _value) {
+            key = _key;
+            value = _value;
+        }
+    }
+
+    public void LRUCache(int _capacity) {
+        capacity = _capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if(map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        }
+        else {
+            return -1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)) {
+            remove(map.get(key));
+        }
+        if(map.size() == capacity) {
+            remove(tail.prev);
+        }
+        insert(new Node(key, value));
+    }
+
+    public void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    public void insert(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
+
+    public static void main(String[] args) {
+        LRU_Cache cache = new LRU_Cache();
+        cache.LRUCache(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1)); // returns 1
+        cache.put(3, 3); // evicts key 2
+        System.out.println(cache.get(2)); // returns -1 (not found)
+        cache.put(4, 4); // evicts key 1
+        System.out.println(cache.get(1)); // returns -1 (not found)
+        System.out.println(cache.get(3)); // returns 3
+        System.out.println(cache.get(4)); // returns 4
+        System.out.println("Done!");
+    }
+}
